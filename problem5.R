ff8edbeacb7f318920097b456e08b5eb0cb76ee8
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-06-15 21:32:13 -0700

    Add files via upload

---
+++ problem5.R
@@ -0,0 +1,9 @@
+
+#all possible values of sum of two dices outcomes
+vals <- c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+#the corresponding probabilities as calculated:
+probs <- c(0.015625,0.03125,0.046875,0.0625,0.078125,
+           0.09375,0.109375,0.125,0.109375,0.09375,
+           0.078125,0.0625,0.046875,0.03125,0.015625)
+#here to calculate the X expected value
+sum(vals*probs)
